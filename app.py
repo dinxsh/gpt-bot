7a5ab0fe2ea0ec382b7c8270d4d0e6fe24367c6a
dinxsh <16219421127@atmiyauni.edu.in>
2023-03-13 14:11:22 +0530

    bug fixes

--- app.py
+++ app.py
@@ -17,17 +17,6 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.slash_command(name="ask-gpt", description="Ask gpt a prompt", guild_ids=[1059369565068476478])
-async def askgpt(interaction: nextcord.Interaction, prompt:str):
-    response = openai.Completion.create(engine="text-davinci-002",prompt=f"{prompt}",max_tokens=2048,temperature=0.5)
-    await interaction.channel.send(response.choices[0].text)
-
-@bot.slash_command(name="ask-a-tsundere", description="ask tsundere a prompt", guild_ids=[1059369565068476478])
-async def asktsundere(interaction: nextcord.Interaction, prompt:str):
-    await interaction.response.defer()
-    response = openai.Completion.create(engine="text-davinci-002",prompt=f"refer to me as oni-chan, {prompt}",max_tokens=2048,temperature=0.5)
-    await interaction.followup.send(content=str(response.choices[0].text)[:1999])
-
 @bot.command()
 async def ask(ctx, *, prompt):
     response = openai.Completion.create(engine="text-davinci-002",prompt=f"{prompt}",max_tokens=2048,temperature=0.5)

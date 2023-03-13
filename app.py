b8937f97b33727f6735ac127c771774c0202f834
dinxsh <16219421127@atmiyauni.edu.in>
2023-03-13 13:10:15 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
-import os
-
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+import discord
+from dotenv import load_dotenv
+import os
+import nextcord
+from nextcord.ext import commands
+TESTING_GUILD_ID = 1059369565068476478  # Replace with your guild ID
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+openai.api_key = OPENAI_KEY
+intents = discord.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
 
+@bot.slash_command(name="ask-gpt", description="Ask gpt a prompt", guild_ids=[1059369565068476478])
+async def askgpt(interaction: nextcord.Interaction, prompt:str):
+    response = openai.Completion.create(engine="text-davinci-002",prompt=f"{prompt}",max_tokens=2048,temperature=0.5)
+    await interaction.channel.send(response.choices[0].text)
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+@bot.slash_command(name="ask-a-tsundere", description="ask tsundere a prompt", guild_ids=[1059369565068476478])
+async def asktsundere(interaction: nextcord.Interaction, prompt:str):
+    await interaction.response.defer()
+    response = openai.Completion.create(engine="text-davinci-002",prompt=f"refer to me as oni-chan, {prompt}",max_tokens=2048,temperature=0.5)
+    await interaction.followup.send(content=str(response.choices[0].text)[:1999])
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+@bot.command()
+async def ask(ctx, *, prompt):
+    response = openai.Completion.create(engine="text-davinci-002",prompt=f"{prompt}",max_tokens=2048,temperature=0.5)
+    await ctx.reply(content=str(response.choices[0].text)[:1999])
+    
+bot.run(TOKEN)
